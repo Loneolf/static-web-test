14e9cfa884362618bb422ca225b6878df96c031b
loneolf <qinghaoqin@163.com>
2023-11-05 14:45:57 +0800

    2

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -3,5 +3,5 @@ import style from "./index.scss";
 
 export default function Home() {
   console.log(style)
-  return <h1 className={style.homeTitle}>Home~~~</h1>;
+  return <h1 className={style.homeTitle}>Home~~~1</h1>;
 }
