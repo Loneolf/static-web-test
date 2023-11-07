9c650d7a9a51e5f02c0eedfb8d7132efa4a12b17
loneolf <qinghaoqin@163.com>
2023-11-07 15:24:43 +0800

    remove TestNum

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -2,6 +2,5 @@ import React from "react";
 import style from "./index.scss";
 
 export default function Home() {
-  console.log(style)
-  return <h1 className={style.homeTitle}>Home~~~18</h1>;
+  return <h1 className={style.homeTitle}>Home</h1>;
 }
